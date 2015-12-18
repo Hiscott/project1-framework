f34ac76e1b2e16625875127dc42060914f7fa719
Hiscott <jjhiscott@gmail.com>
2015-12-18 17:17:28 -0500

    8th

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,13 +1,19 @@
 // Variable Declarations
-var toggleButton = document.getElementsByTagName("button");
-var counter = 0;
-
-
-// add an event listener to the toggle button
-toggleButton.addEventListener("click", toggleButtonClicked);
 
 // toggle button event handler
 function toggleButtonClicked() {
     counter++;
     theCart.innerHTML = counter;
+}
+var counter = 0;
+function organized() {
+    var toggleButton = document.getElementsByTagName("button");
+
+
+
+    // add an event listener to the toggle button
+    for (var i=0; i<toggleButton.length;i++) {
+        addEvent(toggleButton[i], 'click', toggleButtonClicked);
+        toggleButtonClicker();
+}
 }
